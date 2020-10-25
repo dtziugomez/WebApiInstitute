2e7dd1ba405f4f12e6afc0a913fbc049f044df13
dtziugomez <dtziugomez@gmail.com>
2020-10-24 23:01:40 -0500

    Agregar archivos de proyecto.

---
+++ WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace WebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de API web
+
+            // Rutas de API web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "institute/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            //Output format: Json
+            //If you like XML to be return to client, remove below line
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+    }
+}
